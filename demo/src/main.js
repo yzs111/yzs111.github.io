95cecdac39e16db39c49f66a0cdb1838f10a32c9
itcastphpgit1 <1085441412@qq.com>
2020-12-20 21:24:53 +0800

    第二次提交项目到仓库

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,15 +1,10 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css'
+import App from './App.vue'
+import router from './router/'
+
 Vue.config.productionTip = false
-Vue.use(Antd)
-/* eslint-disable no-new */
+
 new Vue({
-  el: '#app',
   router,
-  render: h => h(App)
-})
+  render: h => h(App),
+}).$mount('#app')
--- demo/src/router/index.js
+++ demo/src/router/index.js
@@ -2,25 +2,39 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 // 懒加载
-const Home = () => import('../views/Home')
-const Category = () => import('../views/Category.vue')
-const Cart = () => import('../views/Cart.vue')
-const login = () => import('../views/denglu/login.vue')
+const Home = () => import('../views/home/Home.vue')
+const Category = () => import('../views/category/Category.vue')
+const Cart = () => import('../views/cart/Cart.vue')
+const Me = () => import('../views/me/Me.vue')
 
 Vue.use(VueRouter)
+const routes = [
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    // 指定的组件
+    component:Home
+  },
+  {
+    path: '/category',
+    component:Category
+  },
+  {
+    path: '/cart',
+    component:Cart
+  },
+  {
+    path: '/me',
+    component:Me
+  }
+]
 const router = new VueRouter({
-  routes: [
-    { path: '/', redirect: '/home' },
-    { path: '/login', component: login },
-    {
-      path: '/home',
-      component: Home,
-      children: [
-        { path: '/Cart', component: Cart },
-        { path: '/Category', component: Category }
-      ]
-    }
-  ]
+  routes,
+  // url模式
+  mode:'history'
 })
 
 export default router
